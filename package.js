38ffe02e343a441ac33d75d7b72f330009b41897
Jess Latimer <jplatimer@gmail.com>
2014-08-28 13:02:12 -0700

    updating for 0.9.0

--- package.js
+++ package.js
@@ -1,19 +1,24 @@
 Package.describe({
-	summary: "Upload files to S3. Allows use of Knox Server-Side and get files urls on client-side."
+	summary: "Upload files to S3. Allows use of Knox Server-Side and get files urls on client-side.",
+	git: "https://github.com/manybothans/Meteor-S3-Images-Blaze.git",
+    version: "1.1.0",
+    name: "jplatimer:s3-images-blaze",
 });
+
+/* This defines your actual package */
+Package.onUse(function (api) {
+  // If no version is specified for an 'api.use' dependency, use the
+  // one defined in Meteor 0.9.0.
+  api.versionsFrom('0.9.0');
+
+	api.use(["underscore", "ejson","service-configuration"]);
+	api.use(["ui","templating","spacebars"], "client");
+	api.addFiles("client/blocks.html", "client");
+	api.addFiles("client/events.js", "client");
+	api.addFiles("s3server.js", "server");
+});
+
 
 Npm.depends({
 	knox: "0.8.5"
-});
-
-Package.on_use(function (api) {
-	//Need service-configuration to use Meteor.method
-	api.use(["underscore", "ejson","service-configuration"], ["client", "server"]);
-	api.use(["ui","templating","spacebars"], "client");
-	api.add_files("client/blocks.html", "client");
-	api.add_files("client/events.js", "client");
-	api.add_files("s3server.js", "server");
-
-	//Allows user access to Knox
-	api.export && api.export("Knox","server");
 });
